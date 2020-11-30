566b0ca23b69828023f0e55d22d6e343ca7131da
tmflsby <823246840@qq.com>
2020-11-30 15:48:54 +0800

    datav-report:SalesView组件

--- datav-report/src/components/TodayUsers/index.vue
+++ datav-report/src/components/TodayUsers/index.vue
@@ -18,6 +18,7 @@ export default {
   methods: {
     getOptions() {
       return {
+        tooltip: {},
         xAxis: {
           type: 'category',
           data: [
--- datav-report/src/plugins/element.js
+++ datav-report/src/plugins/element.js
@@ -1,6 +1,11 @@
 import Vue from 'vue'
-import { Row, Col, Card } from 'element-ui'
+import { Row, Col, Card, Menu, MenuItem, RadioGroup, RadioButton, DatePicker } from 'element-ui'
 
 Vue.use(Row)
 Vue.use(Col)
 Vue.use(Card)
+Vue.use(Menu)
+Vue.use(MenuItem)
+Vue.use(RadioGroup)
+Vue.use(RadioButton)
+Vue.use(DatePicker)
