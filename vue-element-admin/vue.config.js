c49e2d7659cb65b7339009e5fb311de7ab742871
tmflsby <823246840@qq.com>
2020-12-09 22:20:36 +0800

    vue-element-admin 接入 数据报表页 update

--- vue-element-admin/vue.config.js
+++ vue-element-admin/vue.config.js
@@ -24,7 +24,7 @@ module.exports = {
    * In most cases please use '/' !!!
    * Detail: https://cli.vuejs.org/config/#publicpath
    */
-  publicPath: '/',
+  publicPath: './',
   outputDir: 'dist',
   assetsDir: 'static',
   lintOnSave: process.env.NODE_ENV === 'development',
@@ -35,7 +35,7 @@ module.exports = {
     overlay: {
       warnings: false,
       errors: true
-    },
+    }
     // before: require('./mock/mock-server.js')
   },
   configureWebpack: {
