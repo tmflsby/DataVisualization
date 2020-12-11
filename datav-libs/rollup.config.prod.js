be6728c0c85796885168ceaf8ccec6d3c73da3ba
tmflsby <823246840@qq.com>
2020-12-11 14:19:50 +0800

    init datav-libs

---
+++ datav-libs/rollup.config.prod.js
@@ -0,0 +1,39 @@
+const path = require('path')
+const resolve = require('rollup-plugin-node-resolve')
+const commonjs = require('rollup-plugin-commonjs')
+const babel = require('rollup-plugin-babel')
+const json = require('rollup-plugin-json')
+const { terser } = require('rollup-plugin-terser')
+const vue = require('rollup-plugin-vue')
+const postcss = require('rollup-plugin-postcss')
+const inputPath = path.resolve(__dirname, './src/index.js')
+const outputUmdPath = path.resolve(__dirname, './dist/datav.main.js')
+const outputEsPath = path.resolve(__dirname, './dist/datav.es.main.js')
+
+module.exports = {
+  input: inputPath,
+  output: [{
+    file: outputUmdPath,
+    format: 'umd',
+    name: 'datav'
+  }, {
+    file: outputEsPath,
+    format: 'es'
+  }],
+  plugins: [
+    resolve(),
+    commonjs(),
+    babel({
+      exclude: 'node_modules/**'
+    }),
+    json(),
+    terser(),
+    vue(),
+    postcss({
+      plugins: []
+    })
+  ],
+  external: [
+    'sam-test-data'
+  ]
+}
